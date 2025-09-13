3887de3bf0f4e0839c3c914e2dff382844a791cd
MP <morephrase@gmail.com>
2025-09-13 23:22:41 +0100

    changes

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 echo "BUILD START"
 pip install -r requirements.txt
 
-# collect static files into staticfiles_build/static
+# collect static files into staticfiles_build
 python3 manage.py collectstatic --noinput --clear
+
+# list collected files for debugging
+echo "STATIC FILES COLLECTED:"
+find staticfiles_build -type f
 
 echo "BUILD END"
