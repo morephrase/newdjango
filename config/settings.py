2b0f51d9e75558a17f23d72d96d33e57c07bfd41
MP <morephrase@gmail.com>
2025-09-15 15:23:15 +0100

    re

--- config/settings.py
+++ config/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         "USER" : "postgres",
-#         "PASSWORD" : "AtuqLZGIjOvGLpVuaJImFVgVhXBLpurp",
-#         "HOST" : "ballast.proxy.rlwy.net",
-#         "PORT" : "57656"
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        "USER" : "postgres",
+        "PASSWORD" : "AtuqLZGIjOvGLpVuaJImFVgVhXBLpurp",
+        "HOST" : "ballast.proxy.rlwy.net",
+        "PORT" : "57656"
+    }
+}
 
 
 
